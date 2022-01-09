382d301daea626491b42eca19e24c94a29936e31
Eric Quiao <quiao.ericfernand@gmail.com>
2022-01-09 22:15:11 +0800

    resolved undisplayed images

--- ctrl.js
+++ ctrl.js
@@ -1,9 +1,9 @@
 const toTop = document.querySelector(".to-top");
 
-window.addEventListener("scroll",() =>{
-  if(window.pageYOffset > 50){
+window.addEventListener("scroll", () => {
+  if (window.pageYOffset > 50) {
     toTop.classList.add("active");
-  }else{
+  } else {
     toTop.classList.remove("active");
   }
 });
@@ -12,60 +12,42 @@ let generate_btn = document.querySelector(".generate_btn");
 
 generate_btn.addEventListener("click", fetchPics);
 
-function fetchPics(){
+function fetchPics() {
+  let countNum = document.querySelector(".countNum h1");
 
-  let countNum = document.querySelector('.countNum h1'); 
-  
   countNum.innerHTML++;
 
   let reset = 0;
   let monitor = countNum.innerHTML;
 
-  console.log(monitor)
+  console.log(monitor);
 
- // let catsImgDiv = document.querySelector(".catsImgDiv")
+  // let catsImgDiv = document.querySelector(".catsImgDiv")
   //catsImgDiv.innerHTML = ''
 
-  fetch('https://api.thecatapi.com/v1/images/search')
-  .then((response) => response.json())
-  .then((data) => {
-    let catsImgUrl = data[0].url
+  fetch("https://api.thecatapi.com/v1/images/search")
+    .then((response) => response.json())
+    .then((data) => {
+      let catsImgUrl = data[0].url;
 
-    console.log(catsImgUrl)
-    let catsImgEl = document.createElement("img")
-    catsImgEl.setAttribute('src', `${catsImgUrl}`)
-   
-    if(monitor==1){
+      console.log(catsImgUrl);
 
-      let catsImgDiv1 = document.querySelector(".catsImgDiv1");
-      catsImgDiv1.appendChild(catsImgEl);
-      console.log(catsImgDiv1)
+      let catsImgEl = document.createElement("img");
+      catsImgEl.setAttribute("src", `${catsImgUrl}`);
+      catsImgEl.setAttribute("width", "auto")
+      
 
-    }else if(monitor==2){
-
-      let catsImgDiv2 = document.querySelector(".catsImgDiv2");
-      catsImgDiv2.appendChild(catsImgEl);
-      console.log(catsImgDiv2)
-
-    }else if(monitor==3){
-      let catsImgDiv3 = document.querySelector(".catsImgDiv3");
-      catsImgDiv3.appendChild(catsImgEl);
-      console.log(catsImgDiv3)
-    }else if(monitor==4){
-      let catsImgDiv4 = document.querySelector(".catsImgDiv4");
-      catsImgDiv4.appendChild(catsImgEl);
-      console.log(catsImgDiv4)
-    }else if(monitor==5){
-      countNum.innerHTML = reset;
-      let catsImgDiv5 = document.querySelector(".catsImgDiv5");
-      catsImgDiv5.appendChild(catsImgEl);
-      console.log(catsImgDiv5)
-    }
-    
-
-  })
-  .catch(err=> console.log(err))
+     if (monitor == 1) {      
+        document.getElementById("placehere1").appendChild(catsImgEl);
+       }
+      else if (monitor == 2) {
+        document.getElementById("placehere2").appendChild(catsImgEl);
+       } 
+      else if (monitor == 3) {
+        countNum.innerHTML = 0;
+        document.getElementById("placehere3").appendChild(catsImgEl);
+       } 
+ 
+     })
+    .catch((err) => console.log(err));
 }
-
-
-
